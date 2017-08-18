57822a8ff826bd80265dc80e4097c64a7f602d72
Stefano Crocco <stefano.crocco@alice.it>
2017-08-18 08:51:41 +0200

    Fix CMakeLists.txt and include files

--- tools/rbrcc/rcc.h
+++ tools/rbrcc/rcc.h
@@ -24,11 +24,11 @@
 #ifndef RCC_H
 #define RCC_H
 
-#include <QtCore/QStringList>
-#include <QtCore/QFileInfo>
-#include <QtCore/QLocale>
-#include <QtCore/QHash>
-#include <QtCore/QString>
+#include <QStringList>
+#include <QFileInfo>
+#include <QLocale>
+#include <QHash>
+#include <QString>
 
 #define TAG_RCC "RCC"
 #define TAG_RESOURCE "qresource"
